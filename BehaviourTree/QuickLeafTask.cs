78e44ff5f63bba9cc391f0f93af11575b3425d16
Paha <Bbenton91@gmail.com>
2018-08-17 01:51:28 -0400

    Added a quick leaf task

---
+++ BehaviourTree/QuickLeafTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BehaviourTree
+{
+    class QuickLeafTask : LeafTask
+    {
+        private Func<BlackBoard, float, BehaviourTreeStatus> fn;
+
+        public QuickLeafTask(Func<BlackBoard, float, BehaviourTreeStatus> fn, string taskName = "") : base(taskName)
+        {
+            this.fn = fn;
+        }
+
+        public override BehaviourTreeStatus Update(BlackBoard blackboard, float delta)
+        {
+            return fn(blackboard, delta);
+        }
+    }
+}
