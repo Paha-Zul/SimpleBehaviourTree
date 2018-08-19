638e491130faec82daa2e83789cbaf1682a63624
Paha <Bbenton91@gmail.com>
2018-08-19 18:28:51 -0400

    Added a ConditionRepeat task

---
+++ BehaviourTree/RepeatUntilCondition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BehaviourTree
+{
+    public class RepeatUntilCondition : Decorator
+    {
+        private Func<BlackBoard, bool> _cond;
+
+        
+        public RepeatUntilCondition(Task child, Func<BlackBoard, bool> cond, string taskName = "") : base(child, taskName)
+        {
+            _cond = cond;
+        }
+        
+        public override BehaviourTreeStatus Update(BlackBoard blackboard, float deltaTime)
+        {
+            //If our condition doesn't pass, return success cause we're done with this condition
+            if (!_cond(blackboard))
+                return BehaviourTreeStatus.Success;
+            
+            //Otherwise, update the child and get it's status
+            var status = childTask.Update(blackboard, deltaTime);
+            //If the status is not success... simply return the status of the child 
+            if (status != BehaviourTreeStatus.Success)
+                return status;
+            
+            //If the child status is success, we need to repeat (so reset() and start()
+            childTask.Reset(blackboard);
+            childTask.Start(blackboard);
+            return BehaviourTreeStatus.Running;
+        }
+    }
+}
