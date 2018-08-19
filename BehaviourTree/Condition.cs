efa49f8beaacfa262d0907fe926e2d529f4aabc7
Paha <Bbenton91@gmail.com>
2018-08-19 17:32:59 -0400

    Added blackboard param in condition

--- BehaviourTree/Condition.cs
+++ BehaviourTree/Condition.cs
@@ -4,15 +4,15 @@ namespace BehaviourTree
 {
     public class Condition : Decorator
     {
-        private Func<bool> _cond;
-        public Condition(Task child, Func<bool> cond, string taskName = "") : base(child, taskName)
+        private Func<BlackBoard, bool> _cond;
+        public Condition(Task child, Func<BlackBoard, bool> cond, string taskName = "") : base(child, taskName)
         {
             _cond = cond;
         }
 
         public override BehaviourTreeStatus Update(BlackBoard blackboard, float deltaTime)
         {
-            return _cond() ? base.Update(blackboard, deltaTime) : BehaviourTreeStatus.Failure;
+            return _cond(blackboard) ? base.Update(blackboard, deltaTime) : BehaviourTreeStatus.Failure;
         }
     }
 }
